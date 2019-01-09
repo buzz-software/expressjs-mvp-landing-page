f6006e55bc2e22caa22397a8a3225d0d39293d4f
Bahadir Balban <Bahadir Balban>
2019-01-08 16:50:37 -0800

    Implement /leads route
    Show a collection of leads upon submission

--- controllers/landing.js
+++ controllers/landing.js
@@ -10,6 +10,12 @@ exports.submit_lead = function(req, res, next) {
 	return models.Lead.create({
 		email: req.body.lead_email
 	}).then(lead => {
-		res.redirect('/');
+		res.redirect('/leads');
+	})
+}
+
+exports.show_leads = function(req, res, next) {
+	return models.Lead.findAll().then(leads => {
+ 		res.render('landing', { title: 'Express', leads: leads });		
 	})
 }
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ let landing = require('../controllers/landing');
 /* GET home page. */
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
+router.get('/leads', landing.show_leads);
 
 module.exports = router;
