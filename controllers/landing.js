fe2844d60647970a2a2a29c2d3b7b6e28a475ed1
Bahadir Balban <Bahadir Balban>
2019-01-09 17:39:11 -0800

    Add lead show edit and post routes

--- controllers/landing.js
+++ controllers/landing.js
@@ -22,8 +22,34 @@ exports.show_leads = function(req, res, next) {
 
 exports.show_lead = function(req, res, next) {
 	return models.Lead.findOne({
-		id : req.params.lead_id
+		where : {
+			id : req.params.lead_id
+		}
 	}).then(lead => {
 		res.render('lead', { lead : lead });
 	});
 }
+
+exports.show_edit_lead = function(req, res, next) {
+	return models.Lead.findOne({
+		where : {
+			id : req.params.lead_id
+		}
+	}).then(lead => {
+		res.render('lead/edit_lead', { lead : lead });
+	});
+}
+
+exports.edit_lead = function(req, res, next) {
+
+	return models.Lead.update({
+		email: req.body.lead_email
+	}, { 
+		where: {
+			id: req.params.lead_id
+		}
+	}).then(result => {
+		res.redirect('/lead/' + req.params.lead_id);
+	})
+}
+
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,6 @@ router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
 router.get('/leads', landing.show_leads);
 router.get('/lead/:lead_id', landing.show_lead);
+router.get('/lead/:lead_id/edit', landing.show_edit_lead);
+router.post('/lead/:lead_id/edit', landing.edit_lead);
 module.exports = router;
