21d45cb85d284fb3e85b70d720fc3676775c28de
Bahadir Balban <Bahadir Balban>
2019-01-17 15:01:27 -0800

    Implement get route for login and sign up

---
+++ controllers/user.js
@@ -0,0 +1,10 @@
+
+
+
+exports.show_login = function(req, res, next) {
+	res.render('user/login', { formData: {}, errors: {} });
+}
+
+exports.show_signup = function(req, res, next) {
+	res.render('user/signup', { formData: {}, errors: {} });
+}
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 let landing = require('../controllers/landing');
+let user = require('../controllers/user');
 
+router.get('/login', user.show_login);
+router.get('/signup', user.show_signup);
 /* GET home page. */
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
